81db689c1127451a4129a650cda370786663b0c5
Anar Abbasov <anar_abbasov@hotmail.com>
2022-06-25 07:07:20 -0700

    added note

--- kitabxana/models.py
+++ kitabxana/models.py
@@ -1,3 +1,17 @@
+from datetime import date
 from django.db import models
 
 # Create your models here.
+
+
+class Note(models.Model):
+    """  
+    id
+    note
+    date
+    user"""
+    #id=models.AutoField(auto_created = True)
+    note=models.CharField(max_length=500)
+    user=models.CharField(max_length=20)
+
+
